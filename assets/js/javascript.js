b089a8096dee7d7f253fbca89360d7936bf51d9c
codeAchsh <code.achsh@gmail.com>
2025-01-04 13:46:50 +0900

    Add files via upload

---
+++ assets/js/javascript.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var navLeftItems = document.getElementById('nav-left-items');
+    navLeftItems.classList.toggle('show');
+    var navRightItems = document.getElementById('nav-right-items');
+    navRightItems.classList.toggle('show');
+}

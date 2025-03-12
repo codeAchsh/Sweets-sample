97ce271363a9c1ee1890be5b310fca8dad5f9170
Chiha <code.achsh@gmail.com>
2025-03-12 23:21:59 +0900

    Updated .gitignore and removed unnecessary files

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 function toggleMenu() {
     var navLeftItems = document.getElementById('nav-left-items');
     navLeftItems.classList.toggle('show');
     var navRightItems = document.getElementById('nav-right-items');
     navRightItems.classList.toggle('show');
+=======
+function toggleMenu() {
+    var navLeftItems = document.getElementById('nav-left-items');
+    navLeftItems.classList.toggle('show');
+    var navRightItems = document.getElementById('nav-right-items');
+    navRightItems.classList.toggle('show');
+>>>>>>> fe19015 (Updated .gitignore and removed unnecessary files)
 }
--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function(){
     // Jquery Methods go
 
@@ -64,3 +65,71 @@ $(function(){
 
 
     
+=======
+$(document).ready(function(){
+    // Jquery Methods go
+
+    // COLLECT HEIGHT AND WIDTH
+    var slideCount = $('#shop-slider ul li').length; //3 number of items
+    // var slideHeight = $('#shop-slider ul li').height(); 
+    var slideWidth = $('#shop-slider ul li').width(); 
+    var currentIndex = 0;
+
+    setInterval(function(){
+        currentIndex = (currentIndex + 1) % slideCount;
+        $('#shop-slider ul').css('transform', 'translateX(' + (-currentIndex * slideWidth) + 'px)');
+    }, 5000);
+    
+
+
+// TABBING
+$(function(){
+    // Hide all tab contents initially
+    $('.tab-content').hide();
+
+    // Show the content of the clicked tab
+    $('#about-btn').click(function(){
+        $('.footer-wrapper').hide();
+        $('#about-content').show();
+    });
+    $('#faq-btn').click(function(){
+        $('.footer-wrapper').hide();
+        $('#faq-content').show();
+    });
+    $('#policy-btn').click(function(){
+        $('.footer-wrapper').hide();
+        $('#policy-content').show();
+    });
+    $('#contact-btn').click(function(){
+        $('.footer-wrapper').hide();
+        $('#contact-content').show();
+    });
+
+    $('.e-ad').click(function(){
+        $('.footer-wrapper').hide();
+        $('#contact-content').show();
+    });
+    $('.sub').click(function(){
+        $('.footer-wrapper').hide();
+        $('#subscribe-content').show();
+    });
+
+
+
+
+    // Add click event to h1 elements to scroll back to footer
+    $('.tab-content h1').click(function(){
+        $('.footer-wrapper').show();
+        document.querySelector('.footer-wrapper').scrollIntoView({behavior:'smooth'})
+        $('.tab-content').hide();
+    });      
+    
+
+});
+
+});
+
+
+
+    
+>>>>>>> fe19015 (Updated .gitignore and removed unnecessary files)
